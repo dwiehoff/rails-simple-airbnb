3f39ae7e3ca009995ca73f63d7d00e1df65947d1
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-11 16:21:33 +0100

    Added Bootstrap and Font-Awesome

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Flat.destroy_all
 
 puts '» Creating flats ...'
 Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
+  name: 'Light & Spacious Garden Dwelling',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
@@ -17,7 +17,7 @@ Flat.create!(
   number_of_guests: 2
 )
 Flat.create!(
-  name: 'Oktoberfest dwelling',
+  name: 'Humble Oktoberfest abode',
   address: 'Bavaria-Ring 20, Munich',
   description: 'Once a year the world travels to Munich to celebrate Oktoberfest. The grounds are pretty much right in the center of this simultaneously traditional and modern city, which makes this place the perfect place to explore during any season.',
   price_per_night: 245,
